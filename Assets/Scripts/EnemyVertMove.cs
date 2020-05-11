58ea538628002a9b7f3f77cce568389279c447c6
Duchi0 <61675359+Duchi0@users.noreply.github.com>
2020-05-10 22:55:07 -0500

    Added animations and enemies

---
+++ Assets/Scripts/EnemyVertMove.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyVertMove : MonoBehaviour
+{
+    private Rigidbody2D enemyRb;
+    private void Start()
+    {
+        enemyRb = GetComponent<Rigidbody2D>();
+    }
+    private void Update()
+    {
+        Move();
+    }
+    private void Move()
+    {
+        enemyRb.velocity = new Vector2(5, 0);
+        enemyRb.velocity = new Vector2(-5, 0);
+    }
+}
