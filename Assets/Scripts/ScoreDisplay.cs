a4a73259d9c34a333118a31087899c41bc400ade
Duchi0 <61675359+Duchi0@users.noreply.github.com>
2020-05-13 12:58:17 -0500

    tried to fix reset score

--- Assets/Scripts/ScoreDisplay.cs
+++ Assets/Scripts/ScoreDisplay.cs
@@ -17,4 +17,5 @@ public class ScoreDisplay : MonoBehaviour
     {
         scoreText.text = $"Score: {scoreManager.score}";
     }
+
 }
--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -15,4 +15,5 @@ public class ScoreManager : MonoBehaviour
     {
         score += increment;
     }
+
 }
